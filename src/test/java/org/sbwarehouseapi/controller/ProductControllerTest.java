52d82868a233551ecc33e17b3be849ed1fc3be05
Timothy Kropp <timothykropp39@gmail.com>
2026-03-14 21:07:46 +0100

    adds tests for ProductController Endpoints

--- src/test/java/org/sbwarehouseapi/controller/ProductControllerTest.java
+++ src/test/java/org/sbwarehouseapi/controller/ProductControllerTest.java
@@ -1,4 +1,51 @@
 package org.sbwarehouseapi.controller;
 
-public class ProductControllerTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ProductController.class)
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void findAllEndpointExistsAndReturnsOk() throws Exception {
+        mockMvc.perform(get("/api/products"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findByIdEndpointExistsAndReturnsOk() throws Exception {
+        mockMvc.perform(get("/api/products/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void createProductEndpointExistsAndReturnsOk() throws Exception {
+        mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                        .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateProductEndpointExistsAndReturnsOk() throws Exception {
+        mockMvc.perform(put("/api/products/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                        .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteProductEndpointExistsAndReturnsOk() throws Exception {
+        mockMvc.perform(delete("/api/products/1"))
+                .andExpect(status().isOk());
+    }
 }
