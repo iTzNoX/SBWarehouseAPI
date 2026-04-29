51300aed1db8756ecdffde735b9a4dd18a4f7a59
Timothy Kropp <timothykropp39@gmail.com>
2026-04-29 13:59:32 +0200

    adds repository

--- src/main/java/org/sbwarehouseapi/repository/ProductRepository.java
+++ src/main/java/org/sbwarehouseapi/repository/ProductRepository.java
@@ -1,4 +1,18 @@
 package org.sbwarehouseapi.repository;
 
-public class ProductRepository {
+import org.sbwarehouseapi.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    long countByNameAndLocation(String name, String location);
+
+    List<Product> findByName(String name);
+
+    List<Product> findByLocation(String location);
+
+    Optional<Product> findByNameAndLocation(String name, String location);
 }
