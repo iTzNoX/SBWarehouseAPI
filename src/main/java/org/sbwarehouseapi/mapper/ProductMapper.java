62c488cd17f8a80314be045bb0320b20aa7e2c4a
Timothy Kropp <timothykropp39@gmail.com>
2026-04-29 13:00:19 +0200

    adds mapper interface

--- src/main/java/org/sbwarehouseapi/mapper/ProductMapper.java
+++ src/main/java/org/sbwarehouseapi/mapper/ProductMapper.java
@@ -1,4 +1,15 @@
 package org.sbwarehouseapi.mapper;
 
-public class ProductMapper {
+import org.sbwarehouseapi.dto.CreateProductDTO;
+import org.sbwarehouseapi.dto.ProductDTO;
+import org.sbwarehouseapi.dto.UpdateProductDTO;
+import org.sbwarehouseapi.entity.Product;
+
+public interface ProductMapper {
+
+    ProductDTO entityToProductDTO(Product product);
+
+    Product createProductDTOToEntity(CreateProductDTO dto);
+
+    void updateEntityFromUpdateProductDTO(UpdateProductDTO dto, Product entity);
 }
