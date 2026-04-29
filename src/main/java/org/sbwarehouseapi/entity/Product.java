db62e059d61e983d5c8662f847de249af10ddf8d
Timothy Kropp <timothykropp39@gmail.com>
2026-04-29 12:53:02 +0200

    adds unique constraint to entity Product

--- src/main/java/org/sbwarehouseapi/entity/Product.java
+++ src/main/java/org/sbwarehouseapi/entity/Product.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "products")
+@Table(name = "products", uniqueConstraints = {@UniqueConstraint(columnNames = {"name", "location"})})
 @Getter
 @Setter
 public class Product {
