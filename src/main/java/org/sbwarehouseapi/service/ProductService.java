d51265de64e4c31b6efc44593bdb1287a040531c
Timothy Kropp <timothykropp39@gmail.com>
2026-04-29 12:58:46 +0200

    adds service interface

--- src/main/java/org/sbwarehouseapi/service/ProductService.java
+++ src/main/java/org/sbwarehouseapi/service/ProductService.java
@@ -1,4 +1,26 @@
 package org.sbwarehouseapi.service;
 
-public class ProductService {
+import org.sbwarehouseapi.dto.CreateProductDTO;
+import org.sbwarehouseapi.dto.ProductDTO;
+import org.sbwarehouseapi.dto.UpdateProductDTO;
+
+import java.util.List;
+
+public interface ProductService {
+
+    void receiveStock(UpdateProductDTO dto);
+
+    void removeStock(UpdateProductDTO dto);
+
+    void transferStock(UpdateProductDTO dto1, UpdateProductDTO dto2);
+
+    List<ProductDTO> findAllProducts();
+
+    ProductDTO findProductById(Long id);
+
+    ProductDTO createProduct(CreateProductDTO dto);
+
+    ProductDTO updateProduct(UpdateProductDTO dto);
+
+    void deleteProduct(Long id);
 }
